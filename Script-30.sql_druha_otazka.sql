69819553f5117ed711eed55dba7a9679b64be630
MarketaAugustinova <marketa.augustinova@email.cz>
2025-10-13 11:43:57 +0200

    Add files via upload

--- Script-30.sql_druha_otazka.sql
+++ Script-30.sql_druha_otazka.sql
@@ -25,3 +25,33 @@ SELECT pc.rok, cpc.name, pc.prum_cena
 FROM prum_ceny pc 
 JOIN czechia_price_category cpc ON pc.category_code = cpc.code
 ORDER BY rok; 
+
+--mleko, chleba
+SELECT pc.rok, cpc.name, pc.prum_cena
+FROM prum_ceny pc 
+JOIN czechia_price_category cpc ON pc.category_code = cpc.code
+WHERE name IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový' )
+ORDER BY rok; 
+
+-- mzdy a ceny vybr. potravin vse
+SELECT *
+FROM prumerne_mzdy_srov_roky pmsr 
+JOIN prum_ceny pc ON pmsr.payroll_year = pc.rok 
+JOIN czechia_price_category cpc ON pc.category_code = cpc.code
+WHERE name IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový' )
+ORDER BY rok;
+
+--srovnani a podil
+SELECT rok, round (AVG(prum_mzda_v_odvetvi)::NUMERIC,2) AS prum_mzda_v_roce, 
+		name, prum_cena,
+		round ((AVG(prum_mzda_v_odvetvi)/prum_cena)::NUMERIC,2) AS pocet_produktu
+FROM prumerne_mzdy_srov_roky pmsr 
+JOIN prum_ceny pc ON pmsr.payroll_year = pc.rok 
+JOIN czechia_price_category cpc ON pc.category_code = cpc.code
+WHERE name IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový' )
+GROUP BY rok, name, pc.prum_cena
+ORDER BY rok;
+
+
+
+
