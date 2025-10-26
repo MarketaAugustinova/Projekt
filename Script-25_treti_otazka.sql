bfee5d62e0d6facae66c28fb5bee757cb86531f9
MarketaAugustinova <marketa.augustinova@email.cz>
2025-10-26 12:29:15 +0100

    Add files via upload

--- Script-25_treti_otazka.sql
+++ Script-25_treti_otazka.sql
@@ -64,7 +64,59 @@ SELECT hodnoceny_rok, COUNT(kvalifikace)
 FROM zdrazeni z
 GROUP BY hodnoceny_rok, kvalifikace   
 HAVING kvalifikace = 'zdrazeni'
-ORDER BY count desc;
+ORDER BY count desc; 
+
+--zdrazeni nad 10 %
+SELECT rok +1 AS hodnoceny_rok, potravina, round(cena_dalsi_rok/prum_cena*100 - 100 ::NUMERIC,0) AS procento_navyseni, 
+	CASE 
+		WHEN  (cena_dalsi_rok/prum_cena*100 - 100) > 10 THEN 'zdrazeni_nad_10_procent'
+		WHEN  (cena_dalsi_rok/prum_cena*100 - 100) > 0 THEN 'zdrazeni'
+		ELSE 'zlevneni' END AS kvalifikace 
+	FROM platy_a_ceny pac
+GROUP BY rok, potravina, cena_dalsi_rok, prum_cena; 
+
+--prumerne zdrazeni vsech potravin
+SELECT hodnoceny_rok, round (AVG(procento_navyseni)::NUMERIC, 1) AS prum_zdrazeni_vsech_potravin
+FROM zdrazeni z 
+GROUP BY hodnoceny_rok
+
+--se mzdama
+SELECT hodnoceny_rok, round (AVG(procento_navyseni)::NUMERIC, 1) AS prum_zdrazeni_vsech_potravin
+FROM zdrazeni z 
+GROUP BY hodnoceny_rok
+ORDER BY prum_zdrazeni_vsech_potravin ; 
+
+--kopie ze scriptu 27 radek 37
+SELECT pac.industry_branch_code, 
+		pac.rok, pac.prum_mzda_v_odvetvi, 
+		pac2.rok, pac2.prum_mzda_v_odvetvi
+FROM platy_a_ceny pac  
+JOIN platy_a_ceny pac2  
+	ON pac.industry_branch_code = pac2.industry_branch_code 
+		AND pac.rok = pac2.rok - 1; 
+
+--procento rozdilu mezd z pac
+--kopie ze scriptu 27 radek 37
+SELECT pac.industry_branch_code, 
+		pac.rok, pac.prum_mzda_v_odvetvi, 
+		pac2.rok, pac2.prum_mzda_v_odvetvi AS pr_mzda_dalsi_rok, 
+		round((pac2.prum_mzda_v_odvetvi/pac.prum_mzda_v_odvetvi*100 - 100)::NUMERIC,2 ) AS procento_zvyseni_mzdy
+FROM platy_a_ceny pac  
+JOIN platy_a_ceny pac2  
+	ON pac.industry_branch_code = pac2.industry_branch_code 
+		AND pac.rok = pac2.rok - 1;
+
+--procento rozdilu mezd z pac
+--kopie ze scriptu 27 radek 37
+SELECT pac.industry_branch_code, 
+		pac.rok, pac.prum_mzda_v_odvetvi, 
+		pac2.rok, pac2.prum_mzda_v_odvetvi AS pr_mzda_dalsi_rok, 
+		round((pac2.prum_mzda_v_odvetvi/pac.prum_mzda_v_odvetvi*100 - 100)::NUMERIC,2 ) AS procento_zvyseni_mzdy
+FROM platy_a_ceny pac  
+JOIN platy_a_ceny pac2  
+	ON pac.industry_branch_code = pac2.industry_branch_code 
+		AND pac.rok = pac2.rok - 1
+GROUP BY pac.industry_branch_code, pac.rok, pac.prum_mzda_v_odvetvi, pac2.rok, pac2.prum_mzda_v_odvetvi;
 
 
 
